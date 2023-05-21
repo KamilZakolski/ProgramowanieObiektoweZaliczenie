efa0076b6286c99ffb5b9b5ca03f0f6898ac2f83
Kamil Zakolski <zakolski@outlook.com>
2023-05-21 20:17:01 +0200

    Dodaj pliki projektów.

---
+++ 1/Osoba.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Osoba
+    {
+        public Osoba(string imięNazwisko)
+        {
+            ImięNazwisko = imięNazwisko;
+        }
+
+        private string imię;
+        public string nazwisko;
+        public DateTime? DataUrodzenia = null;
+        public DateTime? DataŚmierci = null;
+
+        public string Imię
+        {
+            get => imię;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+                else
+                {
+                    imię = value;
+                }
+            }
+        }
+
+        public string ImięNazwisko
+        {
+            get => $"{Imię} {nazwisko}";
+            set
+            {
+                string[] rozbicie = value.Split(' ');
+                Imię = rozbicie[0];
+                nazwisko = (rozbicie.Length > 1) ? rozbicie[^1] : "";
+            }
+        }
+
+        public TimeSpan? Wiek 
+        {
+            get
+            {
+                if (DataUrodzenia == null)
+                {
+                    return null;
+                }
+                if (DataŚmierci == null)
+                {
+                    return DateTime.Today - DataUrodzenia.Value;
+                }
+                else
+                {
+                    return DataŚmierci.Value - DataUrodzenia.Value;
+                }
+            }
+        }
+    }
+}
---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿using _1;
+
+Osoba osoba = new Osoba("Adam Nowak");
+
+Console.WriteLine(osoba.ImięNazwisko);
+Console.WriteLine(osoba.Imię);
+Console.WriteLine(osoba.nazwisko);
+
+osoba.DataUrodzenia = new DateTime(2013, 5, 20);
+Console.WriteLine(osoba.Wiek);
